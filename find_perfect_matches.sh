764c43a35117bbbe444e0741b547cb318939f61e
vishvanata <vishvanata@gatech.edu>
2023-09-12 10:48:32 -0400

    committing#

--- find_perfect_matches.sh
+++ find_perfect_matches.sh
@@ -1,1 +1,7 @@
-oops!
+#!/bin/bash
+queryfile=$1
+subjectfile=$2
+outfile=$3
+blastn -query $queryfile -subject $subjectfile -perc_identity 100 -outfmt 6 -out $outfile
+echo $(cat $outfile | wc -l)
+
