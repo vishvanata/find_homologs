68d66751f4da66f66aa8d204702dfcfaa3b2cac1
vishvanata <vishvanata@gatech.edu>
2023-09-12 10:49:09 -0400

    Added the echo oops command

--- find_perfect_matches.sh
+++ find_perfect_matches.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-queryfile=$1
-subjectfile=$2
-outfile=$3
-blastn -query $queryfile -subject $subjectfile -perc_identity 100 -outfmt 6 -out $outfile
-echo $(cat $outfile | wc -l)
-
+echo "oops!"
